a8b5956f6f38c182e8d4d973c69df20972f67c7d
Chaya Deaver <chayadeaver@gmail.com>
2019-09-19 01:31:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require 'pry'# code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+    @roster[grade] = []
+    @roster[grade] << name
+  else
+    @roster[grade] << name
+  end
+  end
+
+end
